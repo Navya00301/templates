5b5c763b337a2b6d117796072276ccdab3bd6b13
Navya00301 <navyamaria2005@gmail.com>
2026-03-13 16:32:25 +0530

    first commit

--- app6/urls.py
+++ app6/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from.import views
 
 urlpatterns=[
-    path('home/',views.home,name='home'),
+    path('',views.home,name='home'),
     path('login/',views.login,name='login'),
     path('register/',views.register,name='register'),
     path('forgotpassword/',views.forgotpassword,name='forgotpassword'),
--- project6/settings.py
+++ project6/settings.py
@@ -116,3 +116,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "C:/Users/ACER/project6/app6/static",
+]
